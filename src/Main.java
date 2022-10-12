873e72799166dfaa09a33e98f56d298bc7c857b4
Phạm Trung Minh <lminh10bgvn@gmail.com>
2022-10-12 11:42:19 +0700

    3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,7 +6,8 @@ public class Main {
     public static String bill (String cusName, String cusType, double totalValue) {
         double totalPay = totalValue;
         double totalSale = 0;
-        String billDetails = "Customer: " + cusName + "\n";
+        String billDetails = "** BILL DETAILS **\n";
+        billDetails += "Customer: " + cusName + "\n";
         billDetails += "Type: " + cusType + "\n";
 
         if (cusType == "vip1") {
@@ -15,79 +17,71 @@ public class Main {
             totalSale += totalValue * 0.1;
         }
 
-        if (totalValue > 10000) {
+        if (totalValue >= 10000) {
             totalSale += totalValue * 0.1;
-        } else if (totalValue > 5000) {
+        } else if (totalValue >= 5000) {
             totalSale += totalValue * 0.07;
-        } else if (totalValue > 1000) {
-            totalSale += totalValue * 0.01;
+        } else if (totalValue >= 1000) {
+            totalSale += totalValue * 0.05;
         }
 
         totalPay -= totalSale;
 
-        billDetails += "Total Sale: " + totalSale * 1000 + " VND\n";
-        billDetails += "Total Pay: " + totalPay * 1000 + " VND\n";
+        billDetails += "Total Value: " + (int) totalValue * 1000 + " VND\n";
+        billDetails += "Total Sale: " + (int) totalSale * 1000 + " VND\n";
+        billDetails += "Total Pay: " + (int) totalPay * 1000 + " VND\n";
 
         return billDetails;
     }
 
     public static void main(String[] args) {
-//        Customer customer1 = new Customer("Nguyen Van A", 1, "Norm");
-//        Customer customer2 = new Customer("Nguyen Van B", 2, "Vip1");
-//        Customer customer3 = new Customer("Nguyen Van C", 3, "Vip2");
-//        ArrayList<Integer> customers = new ArrayList<>();
-//        customers.add(customer1.getID());
-//        customers.add(customer2.getID());
-//        customers.add(customer3.getID());
+//        double totalValue = 0;
+//        Scanner scanner = new Scanner(System.in);
+//        boolean flag = true;
 //
-//        Product product1 = new Product("Ao", 200, 5);
-//        Product product2 = new Product("Quan dai", 300, 10);
-//        Product product3 = new Product("Quan dui", 150, 20);
-//        Product product4 = new Product("Khan quang", 100, 15);
-//        ArrayList<String> productList = new ArrayList<>();
-//        productList.add(product1.getName());
-//        productList.add(product2.getName());
-//        productList.add(product3.getName());
-//        productList.add(product4.getName());
+//        do {
+//            System.out.print("Product: ");
+//            String product = scanner.next();
+//            double productPrice = 0;
+//
+//            switch (product.toLowerCase()) {
+//                case "ao":
+//                    productPrice = 200;
+//                    break;
+//                case "quan":
+//                    productPrice = 300;
+//                    break;
+//                case "khan quang":
+//                    productPrice = 100;
+//                    break;
+//                default:
+//                    System.out.println("Invalid Product!");
+//                    flag = false;
+//                    break;
+//            }
+//
+//            if(flag) {
+//                System.out.print("Quantity: ");
+//                int quantity = scanner.nextInt();
+//                totalValue += productPrice * quantity;
+//            }
+//
+//            System.out.print("Continue?: ");
+//            String cont = scanner.next();
+//            if (cont.toLowerCase() == "yes") {
+//                flag = true;
+//            } else {
+//                flag = false;
+//            }
+//        } while (flag);
+//
+//        scanner.nextLine();
+//        String customerName = scanner.nextLine();
+//        String customerType = scanner.nextLine();
+//        customerType = customerType.toLowerCase();
+//
+//        System.out.println(bill(customerName, customerType, totalValue));
 
-        double totalValue = 0;
-        Scanner scanner = new Scanner(System.in);
-        boolean flag = true;
-
-        do {
-            System.out.print("Product: ");
-            String product = scanner.next();
-            product = product.toLowerCase();
-            double productPrice = 0;
-
-            switch (product) {
-                case "ao":
-                    productPrice = 200;
-                    break;
-                case "quan":
-                    productPrice = 300;
-                    break;
-                case "khan quang":
-                    productPrice = 100;
-                    break;
-                case "\n":
-                    flag = false;
-                    break;
-                default:
-                    System.out.println("Invalid Product");
-                    flag = false;
-                    break;
-            }
-
-            System.out.print("Quantity: ");
-            int quantity = scanner.nextInt();
-            totalValue += productPrice * quantity;
-        } while (flag);
-
-        String customerName = scanner.next();
-        String customerType = scanner.next();
-        customerType = customerType.toLowerCase();
-
-        System.out.println(bill(customerName, customerType, totalValue));
+        System.out.println(bill("A", "norm", 10000));
     }
 }
